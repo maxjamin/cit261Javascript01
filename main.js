8c370803a0cf92cd42113268bd65d1a189b75c61
maxjamin <maxjamin@gmail.com>
2019-01-13 12:55:38 -0700

    finished with main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ console.log("starting test...");
 
 var numberOfUsers = 15;
 var maxAdditionOfUsers = 10;
+var AverageAge = 27;
 
 /*if statements*/
 if(numberOfUsers < 100)
@@ -16,10 +17,60 @@ for(i=0; i < maxAdditionOfUsers; i++)
 {
 	numberOfUsers += 1;
 }
+console.log( "The additional users + base : ", numberOfUsers);
 
-for(i=0; i < maxAdditionOfUsers; i++)
+
+if(numberOfUsers >= 20 && AverageAge < 30)
 {
-	numberOfUsers02 += 1;
+	console.log("We have a large younge user base!");
 }
 
-console.log( "The additional users: ", numberOfUsers);
+var day;
+switch(new Date().getDay()) {
+  	case 0:
+    	text = "Sunday";
+    	break;
+  	case 1:
+    	text = "Monday";
+    	break;
+  	case 2:
+    	text = "Tuesday";
+    	break;  
+  	case 3:
+    	text = "Wednsday";
+    	break;
+    case 4:
+    	text = "Thursday";
+    	break;
+	case 5:
+    	text = "Friday";
+    	break;
+	case 6:
+    	text = "Saturday";
+    	break;
+}
+console.log("Today is",text);
+
+var clicks =0;
+
+do{
+	console.log("Hit q for exit. ")
+	clicks++;
+
+}
+while(clicks < 13);
+
+console.log("Number of clicks" , clicks, "\n\n");
+
+console.log("end of program");
+
+
+
+
+
+
+
+
+
+
+
